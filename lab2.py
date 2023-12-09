e427bbc4b2e2e2bd84f9568fd6fe7c45dccf70c2
Ilya-Kovalenko <98124003+Ilya-Kovalenko@users.noreply.github.com>
2023-12-09 10:05:31 +0300

    файл для ввода значений

--- lab2.py
+++ lab2.py
@@ -1,19 +1,13 @@
-a = [[1.0, 3.0, 1.0],
-     [3.0, 3.0, 1.0],
-     [5.0, 12.0, 2.0]]
-b = [1.0, 2.0, 3.0]
-
-
-def mat_print(m):
-    for el in m:
-        print(el)
-    print()
+# a = [[1.0, 3.0, 1.0],
+#      [3.0, 3.0, 1.0],
+#      [5.0, 12.0, 2.0]]
+# b = [1.0, 2.0, 3.0]
 
 
 def gauss(matrix, tail):
     n = len(matrix)
 
-    # прямой ход метода Гаусса
+    # прямой ход
     for i in range(n-1):
 
         # нахождение максимального элемента по матрице
@@ -39,23 +33,33 @@ def gauss(matrix, tail):
 
         # приведение к верхнетреугольному виду
         for j in range(i+1, n):
-            ratio = matrix[j][i]  # todo мб сделать ratio = matrix[j][i] / matrix[i][i]
+            ratio = matrix[j][i] / matrix[i][i]
             for k in range(i, n):
-                matrix[j][k] -= matrix[i][k] * (ratio/matrix[i][i])
-            tail[j] -= tail[i] * (ratio/matrix[i][i])  # todo здесь проверить на что делится
+                matrix[j][k] -= matrix[i][k] * ratio
+            tail[j] -= tail[i] * ratio
 
-
-    # обратный ход метода Гаусса
+    # обратный ход
     x = [0] * n
     for i in range(n - 1, -1, -1):
         x[i] = tail[i]
         for j in range(i + 1, n):
             x[i] -= matrix[i][j] * x[j]
-
         x[i] /= matrix[i][i]
 
     return x
 
 
+def read_matrix():
+    matrix = []
+    tail = []
+    with open('input.txt') as file:
+        for line in file.readlines():
+            separated_line = line.split()
+            matrix.append(list(map(float, separated_line[:-1])))
+            tail.append(float(separated_line[-1]))
+    return matrix, tail
+
+
 if __name__ == "__main__":
+    a, b = read_matrix()
     print(gauss(a, b))
